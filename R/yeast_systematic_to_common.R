23a9c3d96cedd517a75254006af4fb08bd278ada
David Gresham <dg107@nyu.edu>
2020-06-30 17:48:18 -0400

    functioning yeast_systematic_to_common function

--- R/yeast_systematic_to_common.R
+++ R/yeast_systematic_to_common.R
@@ -3,14 +3,16 @@
 #' This is  function named 'yeast_systematic_to_common'
 #' which prints takes a systematic yeast gene name and returns the common name.
 #'
+#'
 
-yeast.gene.names <- read.table(file.path(WORKING.PATH, DATA.PATH, GENE.NAME.FILE), sep = "\t", quote="", header=TRUE, stringsAsFactors = FALSE)
-yeast.gene.names[yeast.gene.names == ""] = NA
 
-yeast_systematic_to_common <- function(name.vec) {
-  translated.names <- match(name.vec, yeast.gene.names$SystematicName, nomatch=NA)
-  translated.names <- yeast.gene.names[translated.names, "Name"]
-  no.translation <- is.na(translated.names)
-  translated.names[no.translation] <- name.vec[no.translation]
-  return(translated.names)
+load("data/yeast_gene_names.RData")
+yeast_gene_names[yeast_gene_names == ""] = NA
+
+yeast_systematic_to_common <- function(name_vec) {
+  translated_names <- match(name_vec, yeast_gene_names$SystematicName, nomatch=NA)
+  translated_names <- yeast_gene_names[translated_names, "Name"]
+  no_translation <- is.na(translated_names)
+  translated_names[no_translation] <- name_vec[no_translation]
+  return(translated_names)
 }

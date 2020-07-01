d5c6c749ea3e2679862743e2a3ecb22a4f1838dd
Grace Avecilla <ga824@nyu.edu>
2020-07-01 16:32:39 -0400

    Fixed bug in ueast name changing functions

--- R/yeast_sgd_to_systematic.R
+++ R/yeast_sgd_to_systematic.R
@@ -1,4 +1,4 @@
-#' @name yeast SGD ID to systematic
+#' @name yeast_sgd_to_systematic
 #' @title yeast_sgd_to_systematic
 #'
 #' @param name_vec a vector of SGD IDs (format: SGD:SNNNNNNNNN).
@@ -10,7 +10,8 @@
 load("data/yeast_gene_names.RData")
 
 yeast_sgd_to_systematic <- function(name_vec) {
-  translated_names <- yeast_gene_names$Systematic_name[which(yeast_gene_names$SGD_ID %in% name_vec)]
+  translated_names <- match(name_vec, yeast_gene_names$SGD_ID, nomatch=NA)
+  translated_names <- yeast_gene_names[translated_names, "Systematic_name"]
   no_translation <- is.na(translated_names)
   translated_names[no_translation] <- name_vec[no_translation]
   return(translated_names)

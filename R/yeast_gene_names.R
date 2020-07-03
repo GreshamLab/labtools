d0896014edfc3d20b43b445f745fe6eb096ec7f5
David Gresham <dg107@nyu.edu>
2020-07-03 15:34:47 -0400

    Documented yeast gene names data

---
+++ R/yeast_gene_names.R
@@ -0,0 +1,15 @@
+#' Gene names for 5985 yeast genes.
+#'
+#' A dataset containing the names of 5,895
+#' yeast genes.
+#'
+#' @format A data frame with 5985 rows and 4 variables:
+#' \describe{
+#'   \item{Systematic_name}{systematic name}
+#'   \item{Common_name}{common name}
+#'   \item{GCF_000146045.2_R64_genomic_ID}{Genbank name}
+#'   \item{SGD_ID}{SGD ID}
+#'   ...
+#' }
+#' @source \url{https://www.yeastgenome.org/}
+"yeast_gene_names"
